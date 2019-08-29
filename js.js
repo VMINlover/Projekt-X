34d0c4ec738fbca1132daaf02e2f485351c0919a
Nia2003 <54575500+Nia2003@users.noreply.github.com>
2019-08-29 09:45:51 +0200

    tu

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
 function showLink() {    
     document.getElementById('facebookLink').innerHTML = "https://www.facebook.com/vege.receptz.1";
 }
+function showEmail() {    
+    document.getElementById('email').innerHTML = " VegeRecepty@seznam.cz";
+}
